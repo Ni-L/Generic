55c4c1651d6e7b3905cc6f8778a1238c80d9821b
unknown <nilimawadal1@gmail.com>
2021-05-02 13:22:35 +0530

    [NIlima] Added Maximum Number Finding

---
+++ GenericDemoTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FInd Max Number");
+            int output = MaximumNumber.MaximumIntegerNumber(11,22,33);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+      
+    }
+
+}
